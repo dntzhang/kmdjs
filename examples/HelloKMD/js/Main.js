6c030ca1ad7b77a1766d2f2c93d9505786b873f8
lei <kmdjs@qq.com>
2014-06-28 18:26:55 +0800

    kmdjs v0.0.2
    kmdjs v0.0.2

--- examples/HelloKMD/js/Main.js
+++ examples/HelloKMD/js/Main.js
@@ -7,14 +7,13 @@
     ]
 
 });
-//try to change "define" to "define.build" can build the project.
-define(["Util"], {
+define("Main",["Util"], {
     init: function () {
-            var ball = new Ball(100, 100, 28, 1, 2, "KMD.js");
+            var ball = new Ball(0, 0, 28, 1, -2, "KMD.js");
             var vp = Bom.getViewport();
             setInterval(function () {
                 (ball.x + ball.r * 2 > vp[2] || ball.x < 0) && (ball.vx *= -1);
                 (ball.y + ball.r * 2 > vp[3] || ball.y < 0) && (ball.vy *= -1);
-            }, 100)      
+            }, 100);
     }
-}, true)
+})

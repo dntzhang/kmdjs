9e7de7bb8e36582967653168088549ab02a02184
lei <kmdjs@qq.com>
2014-08-18 10:40:01 +0800

    add combine demo to examples
    add combine demo to examples

---
+++ examples/Combine/js/Main.js
@@ -0,0 +1,20 @@
+﻿kmdjs.config({
+    name:"HelloKMD",
+    baseUrl: "js",
+    classes: [
+        { name: "HelloKMD.Ball"  },
+        { name: "Util.Bom", url: "Util" },
+        { name: "Util.Vector2", url: "Util" }
+    ]
+
+});
+define("Main", {
+    ctor: function () {
+        var ball = new Ball(0, 0, 28, 1, -2, "KMD.js");
+        var vp = Bom.getViewport();
+        setInterval(function () {
+            (ball.x + ball.r * 2 > vp[2] || ball.x < 0) && (ball.vx *= -1);
+            (ball.y + ball.r * 2 > vp[3] || ball.y < 0) && (ball.vy *= -1);
+        }, 30);
+    }
+})

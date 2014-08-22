9cc875e7d7f3a56db7a7d81192060fff40a1a650
lei <kmdjs@qq.com>
2014-08-23 07:06:18 +0800

    remove raphaeljs
    remove raphaeljs

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
         separator: '\r\n'
       },
       dist: {
-          src: ['src/util.js','src/fix.js', 'src/blob.js', 'src/class.js', 'src/js_beautiful.js', 'src/raphael.js', 'src/depTree.js', 'src/request.js', 'src/uglify2.js','src/jslint.js', 'src/core.js'],
+          src: ['src/util.js','src/fix.js', 'src/blob.js', 'src/class.js', 'src/js_beautiful.js',  'src/depTree.js', 'src/request.js', 'src/uglify2.js','src/jslint.js', 'src/core.js'],
         dest: 'dist/kmd.js'
       }
     }

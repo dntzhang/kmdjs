506d60fe58fc98b9392258633206102799279a55
lei <kmdjs@qq.com>
2014-09-09 14:29:25 +0800

    gulp
    use gulp to concat files instead of grunt

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('concat', function () {
+    gulp.src(['src/util.js','src/fix.js', 'src/blob.js', 'src/class.js', 'src/js_beautiful.js',  'src/depTree.js', 'src/request.js', 'src/uglify2.js','src/jslint.js', 'src/core.js'])
+        .pipe(concat('kmd.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['concat']);

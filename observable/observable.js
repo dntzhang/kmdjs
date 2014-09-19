5f5329cad711ee6988e492216f747a8f60c8b13c
lei <kmdjs@qq.com>
2014-09-19 15:22:11 +0800

    observablejs2.0
    observablejs2.0

--- observable/observable.js
+++ observable/observable.js
@@ -1,7 +1,7 @@
 ﻿; (function (win) {
     var initializing=!1,fnTest=/xyz/.test(function(){xyz})?/\b_super\b/:/.*/,__class=function(){};__class.extend=function(n){__class.export=n;function i(){!initializing&&this.ctor&&this.ctor.apply(this,arguments)}var f=this.prototype,u,r,t;initializing=!0,u=new this,initializing=!1;for(t in n)t!="statics"&&(u[t]=typeof n[t]=="function"&&typeof f[t]=="function"&&fnTest.test(n[t])?function(n,t){return function(){var r=this._super,i;return this._super=f[n],i=t.apply(this,arguments),this._super=r,i}}(t,n[t]):n[t]);for(r in this)this.hasOwnProperty(r)&&r!="extend"&&(i[r]=this[r]);if(i.prototype=u,n.statics)for(t in n.statics)n.statics.hasOwnProperty(t)&&(i[t]=n.statics[t],t=="ctor"&&i[t]());return i.prototype.constructor=i,i.extend=arguments.callee,i.implement=function(n){for(var t in n)u[t]=n[t]},i};
 
-    var Observable = __class.extend({
+    var observable = __class.extend({
         "statics": {
             "ctor": function () {
                 this.methods = ["concat", "every", "filter", "forEach", "indexOf", "join", "lastIndexOf", "map", "pop", "push", "reduce", "reduceRight", "reverse", "shift", "slice", "some", "sort", "splice", "unshift", "valueOf"],
@@ -16,28 +16,48 @@
             },
             "isFunction": function (obj) {
                 return this.type(obj) == "function";
+            },
+            "watch": function (target) {
+                new this(target);
             }
         },
-        "ctor": function () {
-            this.observe();
-        },
-        "observe": function () {
-            for (var prop in this) {
-                if (this.hasOwnProperty(prop) && prop != "_super") {
-                    this.watch(this, prop);
+        "ctor": function (target) {
+            for (var prop in target) {
+                if (target.hasOwnProperty(prop)) {
+                    this.watch(target, prop);
                 }
             }
+            if (target.change) throw "naming conflicts！observable will extend 'change' method to your object ."
+            var self = this;
+            target.change = function (fn) {
+                self.propertyChangedHandler = fn;
+            }
         },
-        "change": function (fn) {
-            this.propertyChangedHandler = fn;
-        },
-        "onPropertyChanged": function (prop, value) {
+        "onPropertyChanged": function (prop, value) {          
             this.propertyChangedHandler && this.propertyChangedHandler(prop, value);
+        },
+        "mock": function (target) {
+            var self = this;
+            observable.methods.forEach(function (item) {
+                target[item] = function () {
+                    var result = Array.prototype[item].apply(this, Array.prototype.slice.call(arguments));
+                    for (var cprop in this) {
+                        if (this.hasOwnProperty(cprop) && cprop != "_super" && !observable.isFunction(this[cprop])) {
+                            self.watch(this, cprop);
+                        }
+                    }
+                    if (new RegExp("\\b" + item + "\\b").test(observable.triggerStr)) {
+                        self.onPropertyChanged( "array", item);
+                    }
+                    return result;
+                };
+            });
         },
         "watch": function (target, prop) {
             if (prop.substr(0, 2) == "__") return;
             var self = this;
-            if (Observable.isArray(target) && Observable.isFunction(target[prop])) return;
+            if (observable.isFunction(target[prop])) return;
+            
             var currentValue = target["__" + prop] = target[prop];
             Object.defineProperty(target, prop, {
                 get: function () {
@@ -48,22 +68,13 @@
                     self.onPropertyChanged(prop, value);
                 }
             });
+
+            if (observable.isArray(target)) {
+                this.mock(target);
+            }
             if (typeof currentValue == "object") {
-                if (Observable.isArray(currentValue)) {
-                    Observable.methods.forEach(function (item) {
-                        currentValue[item] = function () {
-                            var result = Array.prototype[item].apply(this, Array.prototype.slice.call(arguments));
-                            for (var cprop in this) {
-                                if (this.hasOwnProperty(cprop) && cprop != "_super" && !Observable.isFunction(this[cprop])) {
-                                    self.watch(this, cprop);
-                                }
-                            }
-                            if (new RegExp("\\b" + item + "\\b").test(Observable.triggerStr)) {
-                                self.onPropertyChanged("array", item);
-                            }
-                            return result;
-                        };
-                    });
+                if (observable.isArray(currentValue)) {
+                    this.mock(currentValue);
                 }
                 for (var cprop in currentValue) {
                     if (currentValue.hasOwnProperty(cprop) && cprop != "_super") {
@@ -74,14 +85,14 @@
         }
     });
 
-    if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = Observable }
-    else if (typeof define === 'function' && define.amd) { define(Observable) }
+    if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = observable }
+    else if (typeof define === 'function' && define.amd) { define(observable) }
     //export to kmd project，以后大家写模块的时候多加下面这几行代码，当耐特在这里谢谢大家了
     else if (typeof define === 'function' && define.kmd) {
-        define("Observable", __class.export);
-        //you can also add any namespace to Observable such as blow code:
-        //define("Util.Observable", __class.export);
+        define("observable", __class.export);
+        //you can also add any namespace to observable such as blow code:
+        //define("Util.observable", __class.export);
     }
-    else { win.Observable = Observable };
+    else { win.observable = observable };
 
 })(Function('return this')());
---
+++ observable/readme.md
@@ -0,0 +1,89 @@
+﻿##写在前面
+各大mv*框架百花齐放，其出现的目的是为了解决以数据模型为中心的程序设计，而非大量过程式的交互效果的实现，所以mv*是专注于数据，而zepto、jquery专注于dom，canvas/svg的库（如kanvas）专注于游戏和报表。他们的使用场景不一样，谁都不会杀死谁，比如一个企业门户，就一个gototop的效果，明显和mv*没有半毛钱关系，这类网站大量存在，jquery/zepto肯定是更好的选择（当然就一gototop首选还是原生js，封一个event和animate）。那么observable.js出现有什么意义？有意义。
+
+##observable.js意义
+
+* 监听任意对象的任意数据变化
+* 让开发者专注于业务逻辑的开发，即数据的操作
+* 更新视图的代码只需写一遍，不再出现在业务逻辑里
+
+当然这里可能有人要吐槽了。尼玛，太落后了，更新视图的逻辑还要自己写！！我用xxx的时候，视图都是自动更新的~~~其实，这里我的观点是：各有优缺点。更新视图的逻辑自己完全控制的话，可以加入自定义的任何特效或者显示逻辑，这样显示逻辑的灵活性得到了很大的提升。如果对显示逻辑要求不是非常高的话，也不愿意手动更新视图的话，完全可以选择observable.js作为mv*的底层的一部分，封装一个mv*.....
+
+##5分钟精通observable.js
+
+这里是没有使用任何模块化开发框架，直接暴露在window下的使用代码：
+
+### 普通对象
+```javascript
+ var User = function (name, age) {
+    this.name = name;
+    this.age = age;
+    //watch User's instance
+    observable.watch(this);
+}
+var user = new User("lisi", 25);
+user.change(function (name, value) {
+    console.log(name + "__" + value);//name__wangwu 
+});
+user.name = "wangwu";
+```
+
+### 对象字面量
+```javascript
+var obj = { a: 1 };
+//watch obj
+observable.watch(obj);
+obj.change(function (name, value) {
+    console.log(name + "__" + value);//a__2 
+});
+obj.a = 2;
+```
+
+### 数组
+```javascript
+var arr = [1, 2, 3];
+//watch obj
+observable.watch(arr);
+arr.change(function (name, value) {
+    //array__push 
+    //3_5
+    console.log(name + "__" + value); 
+});
+arr.push(4);
+arr[3] = 5;
+```
+
+### 复杂对象
+```javascript
+var complexObj = { a: 1, b: 2, c: [{ d: [4] }] };
+//watch complexObj
+observable.watch(complexObj);
+complexObj.change(function (name, value) {           
+    console.log(name + "__" + value);    //d__100 
+});
+complexObj.c[0].d = 100;
+```
+
+### kmdjs兼容性写法
+这里拿Observable 作为例子，以后大家写模块的时候多加下面这几行代码，当耐特在这里谢谢大家了
+
+```javascript
+    if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = Observable }
+    else if (typeof define === 'function' && define.amd) { define(Observable) }
+    //export to kmd project，以后大家写模块的时候多加下面这几行代码，当耐特在这里谢谢大家了
+    else if (typeof define === 'function' && define.kmd) {
+        define("Observable", __class.export);
+        //you can also add any namespace to Observable such as blow code:
+        //define("Util.Observable", __class.export);
+    }
+    else { win.Observable = Observable };
+```
+
+详细代码，请参见：https://github.com/kmdjs/kmdjs/blob/master/observable/observable.js
+
+##有问题反馈
+在使用中有任何问题，欢迎反馈给我，可以用以下联系方式跟我交流
+
+* mail:(kmdjs#qq.com, 把#换成@)
+* qq: 644213943
+* weibo: [@当耐特](http://weibo.com/iamleizhang)

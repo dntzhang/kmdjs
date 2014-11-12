40b790285a922294188bd39c9fb99b90a1131fc0
lei <kmdjs@qq.com>
2014-11-12 15:31:47 +0800

    move to other rep

--- Kanvas/js/UID.js
+++
@@ -1,8 +0,0 @@
-define("Kanvas.UID", {
-    statics: {
-        _nextID: 0,
-        get: function () {
-            return this._nextID++;
-        }
-    }
-})
--- Ket/src/Base/Canvas/DisplayObject.js
+++
@@ -1,7 +0,0 @@
-﻿define("Base.Kanvas.DisplayObject", {
-    ctor: function () {
-        this.alpha=this.scaleX = this.scaleY = 1;
-        this.x = this.y = this.rotation = this.regX = this.regY = this.skewX = this.skewY = 0;
-        this.visible = true; 
-    }
-})

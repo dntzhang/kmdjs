1bf96040c7539b8539f8a3ec1cb7bdd592199983
kmdjs <kmdjs@qq.com>
2016-06-10 18:20:58 +0800

    v0.1.0

--- examples/test/js/pig.js
+++
@@ -1,9 +0,0 @@
-﻿define("Pig:Animal", {
-    ctor: function (age, name) {
-        this._super(age);
-        this.name = name;
-    },
-    climbTree: function () {
-        return "猪不能上树";
-    }
-})
